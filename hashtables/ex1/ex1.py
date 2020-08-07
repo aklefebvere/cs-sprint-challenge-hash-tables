7f542507ba9a365fb97a7571eb58f9e285476e33
aklefebvere <aklefebvere@gmail.com>
2020-08-07 12:51:58 -0400

    Finished SC

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,38 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # if there is no two values to add...
+    if len(weights) == 1:
+        return None
+    # if there is only two values...
+    elif len(weights) == 2:
+        # return the index of the list
+        return (1,0)
+    # if there is more than two elements in the list...
+    else:
+        # create a dict to cache the weights with its indicies
+        weight_dict = {}
+        # list to hold the indicies
+        ans_lst = []
+        # iterate through weights and its index
+        for i, v in enumerate(weights):
+            # create a key off the weight and set the value to its index
+            weight_dict[v] = i
+        # iterate through weights
+        for i in weights:
+            # if limit - i is in the weights dictionary...
+            if limit - i in weight_dict:
+                # then append it to the ans_lst list
+                ans_lst.append(weight_dict[i])
+
+        # if index 1's value is greater than index 0's...
+        if ans_lst[1] > ans_lst[0]:
+            # flip the values
+            ans_lst[0], ans_lst[1] = ans_lst[1], ans_lst[0]
+        # convert the list into a tuple
+        ans_tup = tuple(ans_lst)
+        # return the tuple
+        return ans_tup
+
 
     return None
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,8 +2,25 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    # dictionary to hold list values
+    dict_list = {}
+    # iterate through passed in list
+    for i in a:
+        # put list inside dictionary
+        dict_list[i] = i
+    # list to hold positive numbers that have
+    # corresponding negative numbers
+    result = []
+    # iterate through list
+    for i in a:
+        # if an element in the list is greater than 0...
+        if i > 0:
+            # and if the negative value of the element of the list
+            # is a key in the dictionary...
+            if -i in dict_list:
+                # append the list eleemnt to the result list
+                result.append(i)
+    # return the result list
     return result
 
 
